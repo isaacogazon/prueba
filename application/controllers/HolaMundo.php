1c607ebdfbf1bed57e65e9f0462c94e044c6cb76
Isaac Ogazón Pichardo <isaac_op_92@hotmail.com>
2019-01-11 09:56:44 +0100

    Update HolaMundo.php

--- application/controllers/HolaMundo.php
+++ application/controllers/HolaMundo.php
@@ -29,19 +29,11 @@ class HolaMundo extends CI_Controller {
 	public function menu(){
 		$this->load->helper('url_helper');
 		echo anchor("HolaMundo", "Hola ");
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-		echo anchor("HolaMundo/adios", "Adios cambios prueba 4");
-=======
+
+		echo anchor("HolaMundo/adios", "Adios ultimo cambio");
+
 		echo anchor("HolaMundo/adios", "Adios ");
->>>>>>> parent of 61294ac... Prueba 3
-=======
-		echo anchor("HolaMundo/adios", "Adios ");
->>>>>>> parent of 61294ac... Prueba 3
-=======
-		echo anchor("HolaMundo/adios", "Adios");
->>>>>>> parent of 1f76781... Cambios 4
+
 		echo anchor("HolaMundo/quetal", "Ir a que tal");
 
 		$this->load->view('Menu');
